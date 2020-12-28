ed0d1f559248a3df12260718a308eabc88f386ee
tabarino <tabarino@outlook.com>
2020-12-28 11:36:49 +0000

    How to Query the Store Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-// import { select, Store } from '@ngrx/store';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from './reducers';
 
 @Component({
     selector: 'app-root',
@@ -11,8 +12,13 @@ import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Rout
 })
 export class AppComponent implements OnInit {
     loading = true;
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
-    constructor(private router: Router) { }
+    constructor(
+        private router: Router,
+        private store: Store<AppState>
+    ) { }
 
     ngOnInit() {
         this.router.events.subscribe(event => {
@@ -34,6 +40,14 @@ export class AppComponent implements OnInit {
                 }
             }
         });
+
+        this.isLoggedIn$ = this.store.pipe(
+            map(state => !!state['auth'].user)
+        );
+
+        this.isLoggedOut$ = this.store.pipe(
+            map(state => !state['auth'].user)
+        );
     }
 
     logout() {
