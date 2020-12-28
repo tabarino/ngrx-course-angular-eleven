2cbd8ae11416038de09149cc3d69e70eb3828977
tabarino <tabarino@outlook.com>
2020-12-28 09:36:26 +0000

    The Store Service API - Implementing the Login Screen

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-// import { Store } from '@ngrx/store';
-
-import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
 import { noop } from 'rxjs';
-import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AuthService } from '../auth.service';
+import { AppState } from '../../reducers';
 
 @Component({
     selector: 'login',
@@ -19,8 +18,9 @@ export class LoginComponent implements OnInit {
     constructor(
         private fb: FormBuilder,
         private auth: AuthService,
-        private router: Router) {
-
+        private router: Router,
+        private store: Store<AppState>
+    ) {
         this.form = fb.group({
             email: ['test@angular-university.io', [Validators.required]],
             password: ['test', [Validators.required]]
@@ -31,5 +31,18 @@ export class LoginComponent implements OnInit {
     }
 
     login() {
+        const val = this.form.value;
+        this.auth.login(val.email, val.password)
+            .pipe(
+                tap(user => {
+                    console.log(user);
+                    // this.store.dispatch();
+                    this.router.navigateByUrl('/courses');
+                })
+            )
+            .subscribe(
+                noop,
+                () => alert('Login Failed')
+            );
     }
 }
