e1e76e5ce24055179828322e9b67b8d18cd40a37
tabarino <tabarino@outlook.com>
2020-12-31 17:44:08 +0000

    NgRx Runtime Checks

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,9 +1,11 @@
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
-import { routerReducer } from '@ngrx/router-store';
+import { routerReducer, RouterReducerState } from '@ngrx/router-store';
 import { environment } from '../../environments/environment';
+import { AuthState } from '../auth/reducers';
 
-// tslint:disable-next-line: no-empty-interface
-export interface AppState { }
+export interface AppState {
+    router: RouterReducerState;
+}
 
 export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer
