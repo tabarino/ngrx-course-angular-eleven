a4f6793c32c32c5c4051f2784e88bf865a486e74
tabarino <tabarino@outlook.com>
2021-01-01 19:34:46 +0000

    Loading NgRx Entity Data using a Router Resolver

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -21,6 +21,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesResolver } from './resolvers/courses.resolver';
 // import { EntityDataService, EntityDefinitionService, EntityMetadataMap } from '@ngrx/data';
 import { compareCourses, Course } from './model/course';
 import { compareLessons, Lesson } from './model/lesson';
@@ -28,8 +29,10 @@ import { compareLessons, Lesson } from './model/lesson';
 export const coursesRoutes: Routes = [
     {
         path: '',
-        component: HomeComponent
-
+        component: HomeComponent,
+        resolve: {
+            courses: CoursesResolver
+        }
     },
     {
         path: ':courseUrl',
@@ -71,7 +74,8 @@ export const coursesRoutes: Routes = [
     ],
     entryComponents: [EditCourseDialogComponent],
     providers: [
-        CoursesHttpService
+        CoursesHttpService,
+        CoursesResolver
     ]
 })
 export class CoursesModule {
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -12,8 +12,8 @@ export const reducers: ActionReducerMap<AppState> = {
 
 export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
     return (state, action) => {
-        console.log('state before: ', state);
-        console.log('current action: ', action);
+        // console.log('state before: ', state);
+        // console.log('current action: ', action);
 
         return reducer(state, action);
     };
