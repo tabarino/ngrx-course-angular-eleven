0ec882ba4cd965e9514043a79d12ebed19566e3b
tabarino <tabarino@outlook.com>
2021-01-03 20:10:22 +0000

    Optimistically Editing Entity Data - The Edit Course Dialog

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,9 +1,12 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { Course } from '../model/course';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { Update } from '@ngrx/entity';
 import { Observable } from 'rxjs';
-import { CoursesHttpService } from '../services/courses-http.service';
+import { Course } from '../model/course';
+import { AppState } from '../../reducers';
+import { CourseActions } from '../actions';
 
 @Component({
     selector: 'course-dialog',
@@ -21,7 +24,7 @@ export class EditCourseDialogComponent {
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<EditCourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) data,
-        private coursesService: CoursesHttpService
+        private store: Store<AppState>
     ) {
         this.dialogTitle = data.dialogTitle;
         this.course = data.course;
@@ -34,10 +37,10 @@ export class EditCourseDialogComponent {
             promo: ['', []]
         };
 
-        if (this.mode == 'update') {
+        if (this.mode === 'update') {
             this.form = this.fb.group(formControls);
             this.form.patchValue({ ...data.course });
-        } else if (this.mode == 'create') {
+        } else if (this.mode === 'create') {
             this.form = this.fb.group({
                 ...formControls,
                 url: ['', Validators.required],
@@ -56,9 +59,13 @@ export class EditCourseDialogComponent {
             ...this.form.value
         };
 
-        this.coursesService.saveCourse(course.id, course)
-            .subscribe(
-                () => this.dialogRef.close()
-            );
+        const update: Update<Course> = {
+            id: course.id,
+            changes: course
+        };
+
+        this.store.dispatch(CourseActions.courseUpdated({ update }));
+
+        this.dialogRef.close();
     }
 }
