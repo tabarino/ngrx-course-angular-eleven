d9fadcb341d155ed4e2cfa6e5b60dcc898fbf5ca
tabarino <tabarino@outlook.com>
2021-01-03 20:29:39 +0000

    Optimistic Data Editing - Saving Data in the background with an Effect

--- src/app/courses/effects/courses.effects.ts
+++ src/app/courses/effects/courses.effects.ts
@@ -14,6 +14,16 @@ export class CoursesEffects {
         );
     });
 
+    saveCourse$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(CourseActions.courseUpdated),
+            concatMap(action => this.coursesHttpService.saveCourse(
+                action.update.id,
+                action.update.changes
+            ))
+        );
+    }, { dispatch: false });
+
     constructor(
         private actions$: Actions,
         private coursesHttpService: CoursesHttpService
